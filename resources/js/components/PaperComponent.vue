e4cf06e75a1095494eeae29ac9c73e4aab9b2c16
Watchara-geng <watchara.geng@gmail.com>
2021-11-08 09:53:33 +0700

    api

--- resources/js/components/PaperComponent.vue
+++ resources/js/components/PaperComponent.vue
@@ -1,38 +1,48 @@
 <template >
 <div >
+
     <div class="container">
-        <h2>งานวิจัย</h2>
-        <h6>{{fname}}</h6>
-        <h6>{{lname}}</h6>
-        <img :src="'../'+picture">
-        <h6>{{academic_pos}}</h6>
-        <h6>{{email}}</h6>
+        <div v-if="loading" class="loader">
+            loading.........
+        </div>
+        <div v-else>
+            <h2>งานวิจัย</h2>
+            <h6>{{fname}}</h6>
+            <h6>{{lname}}</h6>
+            <img :src="'../'+picture">
+            <h6>{{academic_pos}}</h6>
+            <h6>{{email}}</h6>
+            <h6>citation_count {{citation_count}}</h6>
+            <h6>document_count {{document_count}}</h6>
+            <h6>cited_by_count {{cited_by_count}}</h6>
+            <h6>hindex {{hindex}}</h6>
 
 <table class="table">
   <thead>
       <tr>
 
       <th scope="col">Paper Name</th>
-      <th scope="col">paper_sourcetitle</th>
-      <th scope="col">paper_doi</th>
-      <th scope="col">paper_volume</th>
-        <th scope="col">paper_page</th>
+      <th scope="col">Year</th>
+      <th scope="col">Journals/Transactions</th>
+      <th scope="col">Doi</th>
+      <th scope="col">Ciations</th>
     </tr>
   </thead>
   <tbody>
 
     <tr v-for="(paper, idx) in papers" :key="idx">
     <td>{{paper.paper_name}}</td>
+
+    <td>{{new Date(paper.paper_yearpub).getFullYear()}}</td>
     <td>{{paper.paper_sourcetitle}}</td>
     <td>{{paper.paper_doi}}</td>
-    <td>{{paper.paper_volume}}</td>
-    <td>{{paper.paper_page}}</td>
+    <td>{{paper.paper_citation}}</td>
 
     </tr>
   </tbody>
 </table>
 </div>
-
+</div>
 
   </div>
 
@@ -47,26 +57,22 @@ export default {
     props:['id'],
     data(){
         return{
+            loading: false,
             fname:'',
             lname:'',
             academic_pos:'',
             email:'',
             picture:'',
+            author_name:'',
+            hindex:'',
+            citation_count:'',
+            cited_by_count:'',
+            document_count:'',
             papers:[]
         }
     },
-    mounted(){
-
-        axios.get('/api/teachers/'+this.id).then(Response=>{
-            var teacher=Response.data.data;
-            this.fname=teacher.fname;
-            this.lname=teacher.lname;
-            this.academic_pos=teacher.academic_pos;
-            this.email=teacher.email;
-            this.picture=teacher.picture;
-            this.papers=teacher.paper;
-        });
-
+    async mounted(){
+        this.getDetail();
     },
     methods:{
         addNewPaperTeacher(){
@@ -75,8 +81,35 @@ export default {
                 lname:this.lname,
                 academic_pos:this.academic_pos,
                 email:this.email,
-
             });
+        },
+        async getDetail(){
+            this.loading = true
+            let resultA = await axios.get('/api/teachers/'+this.id);
+            var teacher=resultA.data.data;
+            let resultB = await axios.get('https://api.elsevier.com/content/search/author?query=authlast('+teacher.lname+')%20and%20authfirst('+teacher.fname+')%20&apiKey=7f59af901d2d86f78a1fd60c1bf9426a');
+            var aid=resultB.data;
+            aid=aid["search-results"]["entry"][0]['dc:identifier'];
+            aid=aid.split(":");
+            aid=aid[1];
+            let resultC = await axios.get('https://api.elsevier.com/content/author?author_id='+aid+'&view=metrics&apiKey=6ab3c2a01c29f0e36b00c8fa1d013f83&httpAccept=application%2Fjson');
+            var data=resultC.data;
+            data=data['author-retrieval-response'][0];
+            this.hindex=data['h-index'];
+            data=data['coredata'];
+            this.fname=teacher.fname;
+            this.lname=teacher.lname;
+            this.academic_pos=teacher.academic_pos;
+            this.email=teacher.email;
+            this.author_name=teacher.author_name;
+            this.picture=teacher.picture;
+            this.papers=teacher.paper;
+            this.citation_count=data['citation-count'];
+            this.cited_by_count=data['citation-count'];
+            this.document_count=data['document-count']
+
+            this.loading = false
+            console.log(aid);
         },
 
 
